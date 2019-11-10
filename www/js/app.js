f878220878beb6b8be97c2f7516e7e2bc8a6b1ba
gasryo <m0971020@gmail.com>
2019-11-10 21:52:54 +0900

    Using Handlebars Templates

--- www/js/app.js
+++ www/js/app.js
@@ -2,7 +2,8 @@
 (function () {
 
     /* ---------------------------------- Local Variables ---------------------------------- */
-    var service = new EmployeeService();
+    var homeTpl = Handlebars.compile($("#home-tpl").html());
+    var employeeListTpl = Handlebars.compile($("#employee-list-tpl").html());
     var service = new EmployeeService();
     service.initialize().done(function () {
         renderHomeView();
@@ -10,28 +11,18 @@
 
 
     /* --------------------------------- Event Registration -------------------------------- */
-    $('.search-key').on('keyup', findByName);
 
     /* ---------------------------------- Local Functions ---------------------------------- */
-    function findByName() {
-        service.findByName($('.search-key').val()).done(function (employees) {
-            var l = employees.length;
-            var e;
-            $('.employee-list').empty();
-            for (var i = 0; i < l; i++) {
-                e = employees[i];
-                $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
-            }
-        });
-    }
 
     function renderHomeView() {
-        var html =
-          "<h1>Directory</h1>" +
-          "<input class='search-key' type='search' placeholder='Enter name'/>" +
-          "<ul class='employee-list'></ul>";
-        $('body').html(html);
+        $('body').html(homeTpl());
         $('.search-key').on('keyup', findByName);
+    }
+
+    function findByName() {
+        service.findByName($('.search-key').val()).done(function (employees) {
+            $('.content').html(employeeListTpl(employees));
+        });
     }
 }());
 
